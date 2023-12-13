cd24f43bebddf4ec841c385f904a03e64ab6cfd8
Henrique Faria <hgf777@gmail.com>
2023-12-13 20:26:58 -0300

    day 9

--- testes/test_fatorial.py
+++ testes/test_fatorial.py
@@ -1,4 +1,5 @@
 import timeit
+from functools import cache
 
 
 # recursive solution
@@ -10,41 +11,67 @@ def fat1(n: int) -> int:
 
 
 # dynamic programming solution
-def fib2(n: int) -> int:
+def fat2(n: int) -> int:
     f = []
     f.append(1)
-    f.append(1)
-    for i in range(2, n + 1):
-        f.append(f[i - 1] + f[i - 2])
+    for i in range(1, n+1):
+        f.append(f[i - 1]*i)
 
     return f[n]
+
+
+# recursive solution
+@ cache
+def fat3(n: int) -> int:
+    if n == 0:
+        return 1
+
+    return n * fat3(n - 1)
 
 
 def recursive_time():
     SETUP_CODE = 'from __main__ import fat1'
 
     TEST_CODE = '''
-n = 900
+n = 400
 fat1(n)
     '''
 
     times = timeit.repeat(setup=SETUP_CODE, stmt=TEST_CODE, repeat=5, number=100)
 
-    print('tempo programa recursivo:', min(times))
+    print(f'tempo programa recursivo: {min(times):.10f}')
 
 
 def dynamic_time():
-    SETUP_CODE = 'from __main__ import fib2'
+    SETUP_CODE = 'from __main__ import fat2'
 
     TEST_CODE = '''
-n = 25
-fib2(n)
+n = 400
+fat2(n)
     '''
 
     times = timeit.repeat(setup=SETUP_CODE, stmt=TEST_CODE, repeat=5, number=100)
 
-    print('tempo programa dinamico:', min(times))
+    print(f'tempo programa dinamico: {min(times):.10f}')
+
+
+def memoization_time():
+    SETUP_CODE = 'from __main__ import fat3'
+
+    TEST_CODE = '''
+n = 400
+fat3(n)
+    '''
+
+    times = timeit.repeat(setup=SETUP_CODE, stmt=TEST_CODE, repeat=5, number=100)
+
+    print(f'tempo programa recursivo com memorização: {min(times):.10f}')
 
 
 if __name__ == '__main__':
+    print('1:', fat1(6))
+    print('2:', fat2(6))
+    print('3:', fat3(6))
     recursive_time()
+    dynamic_time()
+    memoization_time()
--- testes/test_fibonnaci.py
+++ testes/test_fibonnaci.py
@@ -1,4 +1,5 @@
 import timeit
+from functools import cache
 
 
 # recursive solution
@@ -12,7 +13,7 @@ def fib1(n: int) -> int:
 # dynamic programming solution
 def fib2(n: int) -> int:
     f = []
-    f.append(1)
+    f.append(0)
     f.append(1)
     for i in range(2, n + 1):
         f.append(f[i - 1] + f[i - 2])
@@ -20,32 +21,80 @@ def fib2(n: int) -> int:
     return f[n]
 
 
+# recursive solution with memoization
+@cache
+def fib3(n: int) -> int:
+    if n <= 1:
+        return n
+
+    return fib1(n - 1) + fib1(n - 2)
+
+
+# recursive solution with memoization
+def fib4(n: int, r={0: 0, 1: 1}) -> int:
+    if n in r:
+        return r[n]
+    else:
+        r[n] = fib4(n - 1, r) + fib4(n - 2, r)
+        return r[n]
+
+
 def recursive_time():
     SETUP_CODE = 'from __main__ import fib1'
 
     TEST_CODE = '''
-n = 25
+n = 30
 fib1(n)
     '''
 
     times = timeit.repeat(setup=SETUP_CODE, stmt=TEST_CODE, repeat=5, number=100)
 
-    print('tempo programa recursivo:', min(times))
+    print(f'tempo programa recursivo: {min(times):.10f}')
 
 
 def dynamic_time():
     SETUP_CODE = 'from __main__ import fib2'
 
     TEST_CODE = '''
-n = 25
+n = 100
 fib2(n)
     '''
 
     times = timeit.repeat(setup=SETUP_CODE, stmt=TEST_CODE, repeat=5, number=100)
 
-    print('tempo programa dinamico:', min(times))
+    print(f'tempo programa dinamico: {min(times):.10f}')
+
+
+def memoization_time():
+    SETUP_CODE = 'from __main__ import fib3'
+
+    TEST_CODE = '''
+n = 30
+fib3(n)
+    '''
+
+    times = timeit.repeat(setup=SETUP_CODE, stmt=TEST_CODE, repeat=5, number=100)
+
+    print(f'tempo programa recursivo com memorização: {min(times):.10f}')
+
+
+def memoization_dict_time():
+    SETUP_CODE = 'from __main__ import fib4'
+
+    TEST_CODE = '''
+n = 100
+fib4(n)
+    '''
+
+    times = timeit.repeat(setup=SETUP_CODE, stmt=TEST_CODE, repeat=5, number=100)
+
+    print(f'tempo programa recursivo com memorização: {min(times):.10f}')
 
 
 if __name__ == '__main__':
-    recursive_time()
-    dynamic_time()
+    # recursive_time()
+    # dynamic_time()
+    # memoization_time()
+    # memoization_dict_time()
+    print(fib2(500))
+    print(fib4(500))
